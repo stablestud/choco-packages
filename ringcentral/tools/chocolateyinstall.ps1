0deba321803cbb2f180cf2a9ce3a8bfd62930d3e
Alexander Gottweis <stablestud@protonmail.com>
2025-12-12 22:56:22 +0100

    Initial commit

---
+++ ringcentral/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://app.ringcentral.com/download/RingCentral-x64.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url64bit      = $url64
+
+  softwareName  = 'RingCentral'
+
+  checksum64      = 'ff199933ff4e31a7fa44970bdacd880cdce67d28077a4c679ca90ff6b3bd2c56'
+  checksumType64  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
