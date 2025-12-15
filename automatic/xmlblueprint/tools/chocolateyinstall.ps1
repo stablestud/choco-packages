45a252dc424380c28c9b9b8a045ef4ef24559e64
Alexander Gottweis <stablestud@protonmail.com>
2025-12-15 19:05:48 +0100

    Fix xmlblueprint URL to use permanent link not current latest

--- automatic/xmlblueprint/tools/chocolateyinstall.ps1
+++ automatic/xmlblueprint/tools/chocolateyinstall.ps1
@@ -1,25 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32        = 'https://www.xmlblueprint.com/update/download-64bit.php'
-$checksum32   = '50631c91e192c7c7d44a80166702158d39f8e8b4c46f51e5563dca48549c8e95'
-$url64        = 'https://www.xmlblueprint.com/update/download-64bit.php'
-$checksum64   = '50631c91e192c7c7d44a80166702158d39f8e8b4c46f51e5563dca48549c8e95'
+$url        = 'https://filedn.eu/l6hrQdIONMfS36XFW6FwzhS/xmlblueprint-22.2025.10.30.exe'
+$checksum   = '50631c91e192c7c7d44a80166702158d39f8e8b4c46f51e5563dca48549c8e95'
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     unzipLocation = $toolsDir
     fileType      = 'EXE'
-    url           = $url32
-    url64         = $url64
-
+    url           = $url
     softwareName  = 'XMLBlueprint*'
-
-    checksum        = $checksum32
-    checksum64      = $checksum64
-    checksumType    = 'sha256'
-    checksumType64  = 'sha256'
-
+    checksum      = $checksum
+    checksumType  = 'sha256'
     silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-    validExitCodes= @(0)
+    validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
--- automatic/xmlblueprint/update.ps1
+++ automatic/xmlblueprint/update.ps1
@@ -5,19 +5,8 @@ function Get-VersionFromUrl {
         [string]$Url
     )
 
-    # Send a HEAD request and prevent redirects; throw an error if request fails
-    $response = Invoke-WebRequest -Uri $Url -Method Head -MaximumRedirection 0 -ErrorAction SilentlyContinue
-
-    # Check if Location header is missing
-    if (-not $response.Headers["Location"]) {
-        throw "Location header not found in response."
-    }
-
-    # Extract the 'Location' header which contains the final URL after a possible redirect
-    $location = $response.Headers["Location"]
-
     # Extract filename from the Location URL
-    $filename = [System.IO.Path]::GetFileName($location)
+    $filename = [System.IO.Path]::GetFileName($Url)
 
     # Check if the filename matches the expected pattern for version
     if (-not ($filename -match '^xmlblueprint-(\d+\.\d+\.\d+\.\d+)\.exe$')) {
@@ -28,22 +17,39 @@ function Get-VersionFromUrl {
     return $matches[1]
 }
 
+function Get-NextRedirectLocation {
+    param (
+        [string]$Url
+    )
+
+    # Send a HEAD request and prevent redirects; throw an error if request fails
+    $response = Invoke-WebRequest -Uri $Url -Method Head -MaximumRedirection 0 -ErrorAction SilentlyContinue
+
+    # Check if Location header is missing
+    if (-not $response.Headers["Location"]) {
+        throw "Location header not found in response."
+    }
+
+    # Extract the 'Location' header which contains the final URL after a possible redirect
+    $location = $response.Headers["Location"]
+    return $location
+}
+
 function global:au_GetLatest {
     $url = "https://www.xmlblueprint.com/update/download-64bit.php"
-    $version = Get-VersionFromUrl -Url "https://www.xmlblueprint.com/update/download-64bit.php"
+    $download = Get-NextRedirectLocation -Url $url
+    $version = Get-VersionFromUrl -Url $download
 
-    return @{ Version = $version; Url32 = $url; Url64 = $url }
+    return @{ Version = $version; Url = $download }
 }
 
 function global:au_SearchReplace {
     return @{
         "tools\chocolateyinstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.Url32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.Url64)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.Url)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 
-Update-Package -NoReadme
+Update-Package -NoReadme -NoCheckChocoVersion
